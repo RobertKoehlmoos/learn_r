387f29895541ad41407f492ffec03a58a5e9b06d
rob <robert.koehlmoos@gmail.com>
2021-06-24 22:01:38 -0400

    first commit

---
+++ scratch_paper.R
@@ -0,0 +1,6 @@
+# this is a comment
+5%%2 == 1 # two percent signs is how R represents modulo
+die <- 1:6
+a <- 4
+ls # just returning a function causes the interpreter to print out its
+   # it's source code, if available, or the location of it's bytecode
