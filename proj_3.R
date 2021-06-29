5643e51f952712491510c6b53f9c039b2e25f249
rob <robert.koehlmoos@gmail.com>
2021-06-28 21:11:50 -0400

    my score many function handles wildcards beautifly. I'm so proud of it

--- proj_3.R
+++ proj_3.R
@@ -1,5 +1,5 @@
+wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
 get_symbols <- function() {
-  wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
   sample(wheel, size = 3, replace = TRUE,
          prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52))
 }
@@ -81,4 +81,149 @@ for (i in 1:nrow(combos)) {
   combos$prize[i] = score(c(combos$Var1[i], combos$Var2[i], combos$Var3[i]))
 }
 sum(combos$prob * combos$prize) # 0.934356
- 
+
+# Playing with speed
+abs_loop <- function(vec){
+  for (i in 1:length(vec)) {
+    if (vec[i] < 0) {
+      vec[i] <- -vec[i]
+    }
+  }
+  vec
+}
+
+abs_sets <- function(vec){
+  negs <- vec < 0
+  vec[negs] <- vec[negs] * -1
+  vec
+}
+
+long <- rep(c(-1, 1), 5000000)
+system.time(abs_loop(long))
+#user  system elapsed 
+#0.49    0.01    0.50 
+
+system.time(abs_sets(long))
+# user  system elapsed
+# 0.20    0.02    0.21
+
+system.time(abs(long))
+# user  system elapsed 
+# 0.03    0.00    0.03 
+
+# vectorizing functions practice
+change_symbols_old <- function(vec){
+  for (i in 1:length(vec)){
+    if (vec[i] == "DD") {
+      vec[i] <- "joker"
+    } else if (vec[i] == "C") {
+      vec[i] <- "ace"
+    } else if (vec[i] == "7") {
+      vec[i] <- "king"
+    }else if (vec[i] == "B") {
+      vec[i] <- "queen"
+    } else if (vec[i] == "BB") {
+      vec[i] <- "jack"
+    } else if (vec[i] == "BBB") {
+      vec[i] <- "ten"
+    } else {
+      vec[i] <- "nine"
+    } 
+  }
+  vec
+}
+# taking advantage of vectorization
+change_symbols <- function(vec){
+  symbol_map <- c("DD" = "joker", "C" = "ace", "7" = "king", "B" = "queen",
+                  "BB" = "jack", "BBB" = "ten", "0" = "nine")
+  for (symbol in attr(symbol_map, "names")) {
+    new_symbol <- symbol_map[symbol]
+    vec[vec == symbol] <- new_symbol
+  }
+  vec
+}
+# taking advantage of ALL the vectorization
+change_symbols_best <- function(vec) {
+  symbol_map <- c("DD" = "joker", "C" = "ace", "7" = "king", "B" = "queen",
+                  "BB" = "jack", "BBB" = "ten", "0" = "nine")
+  symbol_map[vec]
+}
+vec <- c("DD", "C", "7", "B", "BB", "BBB", "0")
+
+change_symbols(vec)
+##  "joker" "ace"   "king"  "queen" "jack"  "ten"   "nine"
+
+many <- rep(vec, 1000000)
+
+system.time(change_symbols_old(many))
+# user  system elapsed 
+# 8.43    0.00    8.45 
+
+system.time(change_symbols(many))
+# user  system elapsed 
+# 0.30    0.03    0.33 
+
+system.time(change_symbols_best(many))
+# user  system elapsed 
+# 0.11    0.02    0.13 
+
+score_many <- function(syms){
+  # plays_symbols is a n x 3 matrix where each row consists of the three
+  # slots for that play
+  prize <- rep(0, nrow(syms))
+  
+  ## Count the number of cherries and diamonds in each combination
+  cherries <- rowSums(syms == "C")
+  diamonds <- rowSums(syms == "DD")
+  
+  # any cherries means diamonds must be cherries
+  syms[cherries > 0 & syms[, 1] == "DD", 1] <- "C"
+  syms[cherries > 0 & syms[, 2] == "DD", 2] <- "C"
+  syms[cherries > 0 & syms[, 3] == "DD", 3] <- "C"
+  
+  # without cherry rows handles, now diamonds can take the value
+  # of any other non-diamond value in their spin because
+  # either they will all be the same, all will be bars,
+  # or nothing will be give out
+  one_with_two <- syms[, 1] == "DD" & syms[, 2] != "DD"
+  syms[one_with_two, 1] <- syms[one_with_two, 2]
+  one_with_three <- syms[, 1] == "DD" & syms[, 3] != "DD"
+  syms[one_with_three, 1] <- syms[one_with_three, 3]
+  syms[syms[, 2] == "DD", 2] <- syms[syms[, 2] == "DD", 1]
+  syms[syms[, 3] == "DD", 3] <- syms[syms[, 3] == "DD", 1]
+  
+  # case where cherries are present
+  prize <- c(0, 2, 5, 10)[cherries + diamonds + 1]
+  prize[cherries == 0] <- 0
+  
+  # case where all are the same
+  payouts <- c("DD" = 100, "7" = 80, "BBB" = 40, "BB" = 25, 
+               "B" = 10, "C" = 10, "0" = 0)
+  same <- syms[, 1] == syms[, 2] & syms[, 2] == syms[, 3]
+  prize[same] <- payouts[syms[same, 1]]
+  
+  # change prize for combinations that contain all bars
+  bars <- syms == "B" | syms ==  "BB" | syms == "BBB"
+  all_bars <- bars[, 1] & bars[, 2] & bars[, 3] & !same
+  prize[all_bars] <- 5
+  
+  # not need to handle wilds, because there are none left!
+  prize <- prize * (2**diamonds)
+}
+
+get_many_symbols <- function(n) {
+  wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+  vec <- sample(wheel, size = 3 * n, replace = TRUE,
+                prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52))
+  matrix(vec, ncol = 3)
+}
+
+play_many <- function(n) {
+  symb_mat <- get_many_symbols(n = n)
+  results <- data.frame(w1 = symb_mat[,1], w2 = symb_mat[,2],
+             w3 = symb_mat[,3], prize = score_many(symb_mat))
+  print(mean(results$prize))
+  results
+}
+
+system.time(play_many(100000))
